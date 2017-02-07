185b5bb27685c6f474670c8e5f41b38dd43b747b
Austin Smith <austinsmith@me.com>
2017-02-06 20:20:13 -0500

    Update ExportViz.py

--- ExportViz.py
+++ ExportViz.py
@@ -21,7 +21,7 @@ class ExportViz:
 
     Examples
     --------
-	>>> exp = ExportViz('Wikipedia/New/Data/b939c0ade3436e8945a03753d35722de39dfc84a-'+
+	>>> exp = ExportViz('Data/b939c0ade3436e8945a03753d35722de39dfc84a-'+
 	>>> 'a2212e0647d7bc34252dd05124c2d98b3e3120f8.pickle')
 	>>>
 	>>> exp.export_gexf(prune = True)
